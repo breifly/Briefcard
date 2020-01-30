a8c1f36e79d6ec924635c0481d1005340473e25c
florian lahitte <lahitte.florian@gmail.com>
2020-01-29 19:50:59 -0500

    init project

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+// dev.js - don't commit this !!!
+module.exports = {
+  googleClientID:
+    "452358694598-qm4is5549t8qf2fi8vaj94m9hmh3l2f4.apps.googleusercontent.com",
+  googleClientSecret: "OPIOYoiuc_Whq5v1qsABDA_f",
+  mongoURI:
+    "mongodb+srv://Gomeh:xMNMCQVONiTcnILn@briefly-dev-tbixx.mongodb.net/test?retryWrites=true&w=majority",
+  // cookieKey: "rjfhrjvhjrhfjhvlkbjsrklvjbkmlej123fefdgrdfg",
+  // secret: "lfghjhfvljkfbhzrkhjv45454346dfefjazghc",
+  cookieKey: "rjfhsjd78060f7f87kbjsrklvjbkmlej123fefdgrdfg",
+  secret: "lfghjhfvl34738473fbhzr89861khjv45454346dfefjazghc",
+  siteUrl: "http://localhost:3001",
+  linkedinClientID: "86sy0pbld2dtyv",
+  linkedinClientSecret: "UMhL1MEFECFWObdT"
+};
