e6a4e7ffe756c684105e2fb03a21f9ca0839068a
florian lahitte <lahitte.florian@gmail.com>
2020-01-29 20:13:08 -0500

    init heroku

--- config/dev.js
+++ config/dev.js
@@ -1,15 +1,13 @@
 // dev.js - don't commit this !!!
 module.exports = {
   googleClientID:
-    "452358694598-qm4is5549t8qf2fi8vaj94m9hmh3l2f4.apps.googleusercontent.com",
-  googleClientSecret: "OPIOYoiuc_Whq5v1qsABDA_f",
+    '806698189775-k6au71fik9r8pc57ovormng281f7uaik.apps.googleusercontent.com',
+  googleClientSecret: 'RtgZudkUKqw3Q2FRpuirmbGC',
   mongoURI:
-    "mongodb+srv://Gomeh:xMNMCQVONiTcnILn@briefly-dev-tbixx.mongodb.net/test?retryWrites=true&w=majority",
-  // cookieKey: "rjfhrjvhjrhfjhvlkbjsrklvjbkmlej123fefdgrdfg",
-  // secret: "lfghjhfvljkfbhzrkhjv45454346dfefjazghc",
-  cookieKey: "rjfhsjd78060f7f87kbjsrklvjbkmlej123fefdgrdfg",
-  secret: "lfghjhfvl34738473fbhzr89861khjv45454346dfefjazghc",
-  siteUrl: "http://localhost:3001",
-  linkedinClientID: "86sy0pbld2dtyv",
-  linkedinClientSecret: "UMhL1MEFECFWObdT"
+    'mongodb+srv://Gomeh:xMNMCQVONiTcnILn@briefly-dev-tbixx.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'rjfhsjd78060f7f87kbjsrklvjbkmlej123fefdgrdfg',
+  secret: 'lfghjhfvl34738473fbhzr89861khjv45454346dfefjazghc',
+  siteUrl: 'http://localhost:3001',
+  linkedinClientID: '86sy0pbld2dtyv',
+  linkedinClientSecret: 'UMhL1MEFECFWObdT'
 };
