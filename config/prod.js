6b111251e4034dcf8f369f3d214294a4a9061a11
florian lahitte <lahitte.florian@gmail.com>
2020-01-29 20:37:43 -0500

    heroku config 2

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   secret: process.env.SECRET,
-  siteUrl: process.env.SITE_URL
+  siteUrl: process.env.SITE_URL,
   linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
   linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET
 };
