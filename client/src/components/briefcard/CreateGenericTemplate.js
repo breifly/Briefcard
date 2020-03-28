a86a380fc37a275b26ceea85a33173a79a614255
Florian Lahitte <lahitte.florian@gmail.com>
2020-03-27 20:19:12 -0400

    add briefcar

--- client/src/components/briefcard/CreateGenericTemplate.js
+++ client/src/components/briefcard/CreateGenericTemplate.js
@@ -6,14 +6,39 @@ import '../css/CreateGenericTemplate.css';
 import SidebarTemplate from './SidebarTemplate';
 
 class CreateGenericTemplate extends React.Component {
+  state = {
+    getJob: false,
+    newClient: false
+  };
+
+  displayGetJob = () => {
+    this.setState({ getJob: true, newClient: false });
+  };
+
+  displayNewClient = () => {
+    this.setState({ newClient: true, getJob: false });
+  };
+
   render() {
     return (
       <div className="fluid-container">
         <div className="row">
           <div className="col m3 s12 padding-none">
-            <SidebarTemplate />
+            <SidebarTemplate
+              displayGetJob={this.displayGetJob}
+              displayNewClient={this.displayNewClient}
+            />
           </div>
-          <div>bob</div>
+          <div className="col m9 s12">
+            {this.state.getJob ? (
+              <div>
+                <h2>Get Job</h2>
+              </div>
+            ) : (
+              ''
+            )}
+            {this.state.newClient ? <div>New Client</div> : ''}
+          </div>
         </div>
       </div>
     );
--- client/src/components/briefcard/SidebarTemplate.js
+++ client/src/components/briefcard/SidebarTemplate.js
@@ -22,7 +22,42 @@ class sidebarTemplate extends React.Component {
         <ul class="collapsible">
           <li>
             <div class="collapsible-header">
-              <i class="material-icons">filter_drama</i>First
+              <i class="material-icons">filter_drama</i>BriefCard Goal
+            </div>
+            <div class="collapsible-body">
+              <div className="row">
+                <div className="col m4">
+                  <div onClick={this.props.displayGetJob} className="img-block">
+                    <img
+                      src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg"
+                      alt="background"
+                    />
+                    <p className="center white-text">Get Job</p>
+                  </div>
+                </div>
+                <div onClick={this.props.displayNewClient} className="col m4">
+                  <div className="img-block">
+                    <img
+                      src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg"
+                      alt="background"
+                    />
+                    <p className="center white-text">New Client</p>
+                  </div>
+                </div>
+                <div className="col m4">
+                  <div className="img-block">
+                    <img
+                      src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg"
+                      alt="background"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="collapsible-header">
+              <i class="material-icons">place</i>Generic Template
             </div>
             <div class="collapsible-body">
               <span>Lorem ipsum dolor sit amet.</span>
@@ -30,15 +65,7 @@ class sidebarTemplate extends React.Component {
           </li>
           <li>
             <div class="collapsible-header">
-              <i class="material-icons">place</i>Second
-            </div>
-            <div class="collapsible-body">
-              <span>Lorem ipsum dolor sit amet.</span>
-            </div>
-          </li>
-          <li>
-            <div class="collapsible-header">
-              <i class="material-icons">whatshot</i>Third
+              <i class="material-icons">whatshot</i>Block
             </div>
             <div class="collapsible-body">
               <span>Lorem ipsum dolor sit amet.</span>
